13cb6db55c7886c3c6d88f02aad8d39b776e802e
jonasdev <jonaskkristensen@hotmail.dk>
2022-09-18 11:40:54 +0200

    1.0

---
+++ src/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const JSXSchema = (props) => {
+  if (Object.keys(props).length === 0) return null;
+
+  const context = {
+    context: props.context || "https://schema.org/",
+    ...props,
+  };
+
+  const schema = JSON.stringify(context)
+    .replace(/"context"/g, '"@context"')
+    .replace(/"type"/g, '"@type"')
+    .replace(/"id"/g, '"@id"');
+
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{
+        __html: schema,
+      }}
+    />
+  );
+};
